bc93de5a7d89573fd65703f3c3f3a5c1bd842213
Armando Villanueva <armando@havenagency.com>
2017-07-26 16:58:40 -0700

    Added mcrypt and dependencies to box.

--- .provision/bootstrap.sh
+++ .provision/bootstrap.sh
@@ -6,7 +6,7 @@ echo "Installing Git..."
     sudo yum install git -y
 
 echo "Install Dependency Packages for APC..."
-    sudo yum install php56u-pear php56u-devel httpd-devel pcre-devel gcc make
+    sudo yum install -y php56u-pear php56u-devel httpd-devel pcre-devel gcc make
 
 echo "Installing APC(u)..."
     sudo yum install php56u-pecl-apcu
@@ -14,10 +14,12 @@ echo "Installing APC(u)..."
 echo "Installing mod_ssl for apache"
     sudo yum install -y mod_ssl
 
-echo "Installing Xdebug..."
-    sudo yum install php56u-devel
-    sudo yum install php56u-pear
+echo "Installing mcrypt for php5.6..."
+    wget http://http://dl.fedoraproject.org/pub/epel/7/x86_64/l/libmcrypt-2.5.8-13.el7.x86_64.rpm
+    rpm -Uvh libmcrypt-2.5.8-13.el7.x86_64.rpm
+    sudo yum install -y php56u-mcrypt
 
+echo "Installing Xdebug..."
     sudo yum install -y gcc gcc-c++ autoconf automake
 
     pecl install Xdebug
